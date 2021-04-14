85ac021d8004b84c13357968ae557c1f63df2931
unknown <melissa.cinq-mars.1@ulaval.ca>
2021-04-13 22:26:17 -0400

    Add tools plot_hist

--- COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
+++ COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
@@ -1,5 +1,15 @@
+import matplotlib.pyplot as plt
+
 def get_fractal_dimension():
     pass
+
+def plot_meanDisplacement():
+    distance = []
+    plt.hist(distance, bins=20)
+    plt.show()
+
+
+
 
 
 # Écart-type
