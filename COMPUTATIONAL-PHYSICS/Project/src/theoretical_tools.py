74587e85a642d75ccd8be427ad6e8696defa4cc2
unknown <melissa.cinq-mars.1@ulaval.ca>
2021-04-14 16:47:01 -0400

    Add histogram dist

--- COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
+++ COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 import numpy as np
+import matplotlib.mlab as mlab
 
 
 def get_fractal_dimension():
     pass
 
 
-def plot_mean_displacement():
-    distance = []
-    plt.hist(distance, bins=20)
+
+def plot_mean_displacement(distance):
+    plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20)
+    # x = np.linspace(0, 26, 1000)
+    # X = x[:, np.newaxis]
+    # plt.plot(x, pdf(X), )
+    plt.xlabel('Distance moyenne parcourue')
+    plt.ylabel('Probabilité [-]')
     plt.show()
 
 
-x = np.linspace(0, 10, 20)
-
+  #
+  # n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20)
+  #   (mu, sigma) = stats.rayleigh.fit(distance)
+  #   y = mlab.normpdf(bins, mu, sigma)
+  #   l = plt.plot(bins, y, 'r--', linewidth=2)
 
 
 # Écart-type
