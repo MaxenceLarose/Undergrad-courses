a38df2eaf99f796b2aae9b9a650a955dd947648d
maxence larose <maxence.larose.1@ulaval.ca>
2021-04-14 17:57:59 -0400

    Update theoretical_tools.py

--- COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
+++ COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, rayleigh
 import numpy as np
 import matplotlib.mlab as mlab
 
@@ -18,12 +18,18 @@ def plot_mean_displacement(distance):
     plt.ylabel('Probabilité [-]')
     plt.show()
 
+    n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20, density=True)
+    loc, sigma = rayleigh.fit(data=distance)
+    print(f"loc: {loc} and sigma: {sigma}")
+    x = np.linspace(0, distance.max(), 100)
+    pdf_fitted = rayleigh.pdf(x, loc, sigma)
+    plt.plot(x, pdf_fitted, color='r')
+    plt.show()
 
-  #
-  # n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20)
-  #   (mu, sigma) = stats.rayleigh.fit(distance)
-  #   y = mlab.normpdf(bins, mu, sigma)
-  #   l = plt.plot(bins, y, 'r--', linewidth=2)
+    # n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20)
+    # (mu, sigma) = rayleigh.fit(distance)
+    # y = mlab.normpdf(bins, mu, sigma)
+    # l = plt.plot(bins, y, 'r--', linewidth=2)
 
 
 # Écart-type
