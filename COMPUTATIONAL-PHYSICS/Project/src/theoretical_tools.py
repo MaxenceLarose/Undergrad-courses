bc3d4aebabe04e716379e7631ac31937bacc2ae7
maxence larose <maxence.larose.1@ulaval.ca>
2021-04-15 11:54:37 -0400

    Update theoretical_tools.py

--- COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
+++ COMPUTATIONAL-PHYSICS/Project/src/theoretical_tools.py
@@ -11,20 +11,22 @@ def get_fractal_dimension():
 
 def plot_mean_displacement(distance, nb_steps, number_of_walkers):
     #plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20)
-    n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=20, density=True)
+    n, bins, patches = plt.hist(distance, histtype='stepfilled', alpha=0.5, bins=30, density=True)
     loc, sigma = rayleigh.fit(data=distance)
     print(f"loc: {loc} and sigma: {sigma}")
     x = np.linspace(0, distance.max()+5, 100)
     pdf_fitted = rayleigh.pdf(x, loc, sigma)
     plt.plot(x, pdf_fitted, color='r')
     rms = np.sqrt(nb_steps)
-    plt.vlines(rms, 0, pdf_fitted.max(), linestyle='--', color='k')
+    rms_ex = np.sqrt(np.mean(np.asarray(distance)**2))
+    plt.vlines(rms, 0, pdf_fitted.max(), linestyle='--', color='k', linewidth=1)
+    plt.vlines(rms_ex, 0, pdf_fitted.max(), linestyles='--', color='grey', linewidth=1)
     plt.xlabel('Distance moyenne parcourue')
-    plt.ylabel('Fréquence')
+    plt.ylabel('Probabilité [-]')
     plt.title('Distance moyenne parcourue pour {} marches aléatoires '.format(number_of_walkers))
-    plt.legend(['Rayleigh $\sigma =$ {}'.format(sigma), '$N$ = {}'.format(number_of_walkers), 'RMS = {}'. format(rms)])
+    plt.legend(['Rayleigh $\sigma =$ {}'.format(sigma), '$N$ = {}'.format(nb_steps), 'RMS = {}'. format(rms),
+                'RMS (exp) = {}'.format(rms_ex)])
     plt.show()
-
 
 
 
